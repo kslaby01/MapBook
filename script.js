44f01f70e5bce3698ad84e2fdc7fd5cd2b298682
kslaby01 <kenneth.slaby@tufts.edu>
2017-03-22 18:44:29 -0400

    Hexagon color selection

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-
-
+var DEFAULT_SQUARE_WIDTH = 60;
+var DEFAULT_RADIUS = 13.5;
+var VISITING = false;
+var WANTING = false;
   
 var data, options, chart;
 var neutralColor = '#F5F5F5'; // 0 grey
-var visitedColor = '#0B00B9'; // 1 green
-var wntVisitColor = '#00B90D'; // 2 blue
+var neutral = 0;
+var visitedColor = '#00B90D'; // 1 blue
+var visited = 1;
+var wntVisitColor = '#0B00B9'; // 2 green 
+var want = 2;
 
 
 google.charts.load("visualization", "1", {packages:["geochart"]});
@@ -12,7 +17,9 @@ google.charts.setOnLoadCallback(drawRegionsMap);
 
 
 
-  function drawRegionsMap() {
+// create map
+
+function drawRegionsMap() {
 
       data = google.visualization.arrayToDataTable([
         ['State', 'Visited', {role: 'tooltip', p: {html: 'true'}}],
@@ -72,7 +79,7 @@ google.charts.setOnLoadCallback(drawRegionsMap);
         displayMode: 'regions',
         resolution:'provinces',
         colorAxis:{
-          colors:[neutralColor, visitedColor, wntVisitColor],
+          colors:[neutralColor, wntVisitColor, visitedColor],
           minValue: 0,
           maxValue:2},
        region:'US',
@@ -83,26 +90,19 @@ google.charts.setOnLoadCallback(drawRegionsMap);
           for (var i = 0; i < data.getNumberOfRows(); i++) {
                 var selection = chart.getSelection()[0];
                 if (i === selection.row) {
-                  console.log(i + " selection: " + selection.row);
-                  dealwithModal(i);
+                  updateColors(i);
                   break;
-                }
-                
+                }  
           }
-          chart.draw(data, options);
-        }
-
-      function dealwithModal(i) {
-          $('#visitModal').modal('show');
-          document.getElementById("btn-close").onclick = function() {updateColors(i)};
-          return;
       }
 
       chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
       google.visualization.events.addListener(chart, 'select', toggleVisit);
 
-     chart.draw(data, options);
-  }
+      chart.draw(data, options);
+}
+
+// Update the tally of the visited
 
 function updateCounts()
 {
@@ -115,9 +115,6 @@ function updateCounts()
       wntVisitCount++;
     } 
   }
-  //$('#checkbox').prop('checked', true); // Checks it
-  $('#visitedChbx').prop('checked', true); // Unchecks it
-  $('#wntvisitChbx').prop('checked', false); // Unchecks it
 
   document.getElementById("visited_num").innerHTML = visitedCount;
   document.getElementById("want_num").innerHTML = wntVisitCount;
@@ -126,21 +123,109 @@ function updateCounts()
 }
         
 
+// update colors on the map
 
 function updateColors(i) {
-  if($("#visitedChbx").is(':checked')){
-  // visited color
-    data.setValue(i, 1, 2);
-  // Code in the case checkbox is checked.
-  } else if ($("#wntvisitChbx").is(':checked')) {
-  // want to visit color
-    data.setValue(i, 1, 1);
-   // Code in the case checkbox is NOT checked.
-  } else {
-  //default color
-    data.setValue(i, 1, 0);
+  if(VISITING){
+    data.setValue(i, 1, 2);  // visited color
+  } else if (WANTING) {
+    data.setValue(i, 1, 1);  // want to visit color
+  } else { // NO HEX SELECTED
+    data.setValue(i, 1, 0);  //default color
   }
   chart.draw(data, options);
   updateCounts();
   return;
 }
+
+
+// Hexagon color changing
+var hexagon1, hexagon2, layer1, layer2;
+window.onload = function() {
+    $("#helpModal").modal()
+    var stage1 = new Konva.Stage({
+        container: 'shape_container_v',
+        width: DEFAULT_SQUARE_WIDTH,
+        height: DEFAULT_SQUARE_WIDTH
+    });
+      var stage2 = new Konva.Stage({
+        container: 'shape_container_w',
+        width: DEFAULT_SQUARE_WIDTH,
+        height: DEFAULT_SQUARE_WIDTH
+    });
+
+    layer1 = new Konva.Layer();
+    layer2 = new Konva.Layer();
+
+    hexagon1 = new Konva.RegularPolygon({
+        x: stage1.getWidth() / 2,
+        y: stage1.getHeight() / 2,
+        sides: 6,
+        radius: DEFAULT_RADIUS,
+        fill: visitedColor,
+        strokeWidth: 0
+    });
+    hexagon2 = new Konva.RegularPolygon({
+        x: stage2.getWidth() / 2,
+        y: stage2.getHeight() / 2,
+        sides: 6,
+        radius: DEFAULT_RADIUS,
+        fill: wntVisitColor,
+        strokeWidth: 0,
+        align: 'top'
+    });
+    hexagon1.on('click touchstart', function() {
+        var rad = this.radius() == 2*DEFAULT_RADIUS ? DEFAULT_RADIUS : 2*DEFAULT_RADIUS;
+        this.radius(rad);
+
+        if (rad == 2*DEFAULT_RADIUS) {
+            VISITING = true;
+            WANTING = false;
+            hexagon2.radius(DEFAULT_RADIUS);
+            layer2.draw();
+        } else {
+          VISITING = false;
+        }
+        layer1.draw();
+    });
+
+    hexagon2.on('click touchstart', function() {
+        var rad = this.radius() == 2*DEFAULT_RADIUS ? DEFAULT_RADIUS : 2*DEFAULT_RADIUS;
+        this.radius(rad);
+
+        if (rad == 2*DEFAULT_RADIUS) {
+            VISITING = false;
+            WANTING = true;
+            hexagon1.radius(DEFAULT_RADIUS);
+            layer1.draw();
+        } else {
+          WANTING = false;
+        }
+        layer2.draw();
+    });
+
+    layer1.add(hexagon1);
+    layer2.add(hexagon2);
+    stage1.add(layer1);
+    stage2.add(layer2);
+
+    var period = 2000;
+};
+
+function hexColorUpdate(jscolor, type) {
+      //test(jscolor
+      if (type == 'visit'){
+          visitedColor = '#' + jscolor;
+          hexagon1.fill(visitedColor);
+          layer1.draw();
+          options['colorAxis']['colors'][2] = visitedColor;
+          chart.draw(data, options);
+      } else if (type == 'want') {
+          wntVisitColor = '#' + jscolor;
+          hexagon2.fill(wntVisitColor);
+          layer2.draw();
+          options['colorAxis']['colors'][1] = wntVisitColor;
+          chart.draw(data, options);
+      }
+}
+
